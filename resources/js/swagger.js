a3e321eff00305882a013be2ecb319ba45b1a40b
inokab <inokabalazs@gmail.com>
2023-04-25 09:23:30 +0200

    add docker-compose.yml

--- resources/js/swagger.js
+++ resources/js/swagger.js
@@ -3,5 +3,5 @@ import 'swagger-ui/dist/swagger-ui.css';
 
 SwaggerUI({
     dom_id: '#swagger-api',
-    url: 'http://localhost:8000/openapi.yaml',
+    url: 'http://127.0.0.1:8000/openapi.yaml',
 });
